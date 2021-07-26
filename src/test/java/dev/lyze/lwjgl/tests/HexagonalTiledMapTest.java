9b6147ee711edc945a20a9ed126297d28dd533ec
Lyze <11274700+lyze237@users.noreply.github.com>
2021-07-26 22:05:15 +0200

    ✅ Added more hex tests

--- src/test/java/dev/lyze/lwjgl/tests/HexagonalTiledMapTest.java
+++ src/test/java/dev/lyze/lwjgl/tests/HexagonalTiledMapTest.java
@@ -8,13 +8,73 @@ import org.junit.jupiter.api.Test;
 public class HexagonalTiledMapTest extends LwjglTiledMapPixmapCompareTest {
     @Test
     @Tag("lwjgl")
-    public void fourByFour() throws InterruptedException {
-        compareMap("4x4", "Hex/4x4.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/4x4.png"));
+    public void fourByFourOddY() throws InterruptedException {
+        compareMap("4x4", "Hex/4x4-odd-y.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/4x4-odd-y.png"));
     }
 
     @Test
     @Tag("lwjgl")
-    public void threeByThree() throws InterruptedException {
-        compareMap("3x3", "Hex/3x3.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/3x3.png"));
+    public void fourByFourOddX() throws InterruptedException {
+        compareMap("4xa", "Hex/4x4-odd-x.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/4x4-odd-x.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void fourByFourEvenY() throws InterruptedException {
+        compareMap("4x4", "Hex/4x4-even-y.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/4x4-even-y.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void fourByFourEvenX() throws InterruptedException {
+        compareMap("4x4", "Hex/4x4-even-x.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/4x4-even-x.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void threeByThreeOddY() throws InterruptedException {
+        compareMap("3x3", "Hex/3x3-odd-y.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/3x3-odd-y.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void threeByThreeOddX() throws InterruptedException {
+        compareMap("3x3", "Hex/3x3-odd-x.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/3x3-odd-x.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void threeByThreeEvenX() throws InterruptedException {
+        compareMap("3x3", "Hex/3x3-even-x.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/3x3-even-x.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void threeByThreeEvenY() throws InterruptedException {
+        compareMap("3x3", "Hex/3x3-even-y.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/3x3-even-y.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void fiftyByFifty() throws InterruptedException {
+        compareMap("50x50", "Hex/50x50.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/50x50.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void scholz() throws InterruptedException {
+        compareMap("Scholz", "Hex/Scholz.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/Scholz.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void scholzTileSideFour() throws InterruptedException {
+        compareMap("Scholz", "Hex/Scholz-4.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/Scholz-4.png"));
+    }
+
+    @Test
+    @Tag("lwjgl")
+    public void scholzTileSideSixteen() throws InterruptedException {
+        compareMap("Scholz", "Hex/Scholz-16.tmx", TiledMapRendererType.Hex, Gdx.files.internal("Hex/Scholz-16.png"));
     }
 }
